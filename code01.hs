5134598470224bc55907fc59663537733d20c085
gbagan <guillaume.bagan@liris.cnrs.fr>
2021-12-02 20:57:45 +0100

    Update code01.hs

--- code01.hs
+++ code01.hs
@@ -1,10 +1,10 @@
 -- https://adventofcode.com/2021/day/1
 module Main where
 
-countAug :: Int -> [Int] -> Int
-countAug n l = length . filter id $ zipWith (<) l (drop n l)
+algo :: Int -> [Int] -> Int
+algo n l = length . filter id $ zipWith (<) l (drop n l)
 
 main = do
     xs <- map read . lines <$> readFile "data01"
-    print $ countAug 1 xs
-    print $ countAug 3 xs
+    print $ algo 1 xs
+    print $ algo 3 xs
