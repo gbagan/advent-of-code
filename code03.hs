03cec196ebf6e3487ee2ca68a31dbc9afd224f4e
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2021-12-03 12:21:34 +0100

    day 3

---
+++ code03.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE BlockArguments #-}
+
+module Main where
+import Data.Char (digitToInt)
+import Data.List (foldl', (!!))
+
+gammaRate :: [String] -> String
+gammaRate l = map (\v -> if v >= ndiv2 then '1' else '0')
+            . foldl' (zipWith \x y -> x + digitToInt y) (repeat 0)
+            $ l
+        where ndiv2 = length l `div` 2
+
+toDec :: String -> Int
+toDec = foldl' (\acc x -> acc * 2 + digitToInt x) 0
+
+algo1 :: [String] -> Int
+algo1 l = toDec e * toDec g where
+    g = gammaRate l
+    e = map (\x -> if x == '0' then '1' else '0') g
+
+data Common = MostCommon | LeastCommon deriving (Eq)
+
+filterCode :: Common -> [String] -> String
+filterCode common l = go 0 l where
+    go _ [] = undefined
+    go _ [x] = x
+    go i xs =
+        let n = length xs
+            ys = filter (\x -> x !! i == '1') xs
+            zs = filter (\x -> x !! i == '0') xs 
+        in
+        if (length ys >= length zs) == (common == MostCommon) then
+            go (i + 1) ys
+        else
+            go (i + 1) zs
+
+algo2 :: [String] -> Int
+algo2 l = toDec x * toDec y where
+    x = filterCode MostCommon l
+    y = filterCode LeastCommon l
+
+main = do
+    xs <- lines <$> readFile "data03"
+    print $ algo1 xs
+    print $ algo2 xs
