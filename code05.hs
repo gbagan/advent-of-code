a23bb299581c5bc246a3da6bfcc21fd2ffcdd4b5
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2021-12-05 14:19:07 +0100

    clean code of day 5

--- code05.hs
+++ code05.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE MultiWayIf #-}
 
 module Main where
 import Data.Maybe (mapMaybe)
@@ -24,22 +25,24 @@ playOn :: (Int, Int) -> Board -> Board
 playOn (x, y) = Map.insertWith (+) (x, y) 1
 
 fillBoard1 :: Bool -> Line -> Board -> Board
-fillBoard1 diag (Line (x1, y1) (x2, y2)) board
-    | y1 == y2  = foldl' (flip playOn) board ((,y1) <$> [min x1 x2..max x1 x2])
-    | x1 == x2  = foldl' (flip playOn) board ((x1,) <$> [min y1 y2..max y1 y2])
-    | diag      = let len = abs (x1 - x2)
-                      dx = signum (x2 - x1)
-                      dy = signum (y2 - y1)
-                      move (x, y) = (x + dx, y + dy)
-                  in foldl' (flip playOn) board $ take (len+1) (iterate move (x1, y1))
-    | otherwise = board
+fillBoard1 diag (Line (x1, y1) (x2, y2)) board =
+    let points =
+         if | y1 == y2  -> (,y1) <$> [min x1 x2..max x1 x2]
+            | x1 == x2  -> (x1,) <$> [min y1 y2..max y1 y2]
+            | diag      -> let len = abs (x1 - x2)
+                               dx = signum (x2 - x1)
+                               dy = signum (y2 - y1)
+                               move (x, y) = (x + dx, y + dy)
+                           in take (len+1) (iterate move (x1, y1))
+            | otherwise -> []
+    in foldl' (flip playOn) board points
 
 fillBoard :: Bool -> [Line] -> Board
-fillBoard diag = foldl' (flip (fillBoard1 diag)) Map.empty
+fillBoard diag = foldl' (flip $ fillBoard1 diag) Map.empty
 
 countIntersections :: Bool -> [Line] -> Int
 countIntersections diag ls = 
-    length . filter (\(k, v) -> v > 1) . Map.toList . fillBoard diag $ ls
+    length . filter (\(_, v) -> v > 1) . Map.toList . fillBoard diag $ ls
 
 main = do
     ls <- mapMaybe (=~ line) . lines <$> readFile "data05"
