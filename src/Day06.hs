d525687d751bc5f1e0d786588ed9352010187dfd
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2021-12-06 15:39:05 +0100

    update Day06.hs

--- src/Day06.hs
+++ src/Day06.hs
@@ -11,8 +11,8 @@ buildVec xs = Vec (f 0) (f 1) (f 2) (f 3) (f 4) (f 5) (f 6) (f 7) (f 8)
 
 algo :: Int -> Vec -> Int
 algo n = vecSum . (!!n) . iterate' step where
-      vecSum (Vec x0 x1 x2 x3 x4 x5 x6 x7 x8) = x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8          
-      step (Vec x0 x1 x2 x3 x4 x5 x6 x7 x8) = Vec x1 x2 x3 x4 x5 x6 (x0+x7) x8 x0
+            vecSum (Vec x0 x1 x2 x3 x4 x5 x6 x7 x8) = x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8          
+            step (Vec x0 x1 x2 x3 x4 x5 x6 x7 x8) = Vec x1 x2 x3 x4 x5 x6 (x0+x7) x8 x0
 
 solve :: String -> Maybe (Int, Int)
 solve s = do
