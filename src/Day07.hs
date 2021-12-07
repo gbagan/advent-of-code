6a8650cb290eccf501b3a69db03acff6c9dc64fe
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2021-12-07 15:07:59 +0100

    day 7 simplify the code

--- src/Day07.hs
+++ src/Day07.hs
@@ -5,17 +5,12 @@ import Text.Read (readMaybe)
 import Util (average)
 
 solve1 :: [Int] -> Int
-solve1 xs =
-    let m = xs !! (length xs `div` 2)
-    in sum [abs (x - m) | x <- xs]
+solve1 xs = sum [abs (x - m) | x <- xs] where
+        m = xs !! (length xs `div` 2)
 
 solve2 :: [Int] -> Int
-solve2 xs = min t1 t2 where
-        m = average xs
-        m1 = minimum . filter ((>=m) . realToFrac) $ xs
-        m2 = maximum . filter ((<=m) . realToFrac) $ xs
-        t1 = sum [bin . abs $ x - m1 | x <- xs]
-        t2 = sum [bin . abs $ x - m2 | x <- xs]
+solve2 xs = sum [bin . abs $ x - m | x <- xs] where
+        m = floor (average xs)
         bin n = n * (n + 1) `div` 2
 
 solve :: String -> Maybe (Int, Int)
