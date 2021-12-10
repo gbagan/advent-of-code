1dc7a2f8943ccf43d90ee9a24c9d0dd8f23a0ba6
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2021-12-10 18:28:09 +0100

    update Day 10

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,38 +1,30 @@
 module Day10 where
 import Data.List (foldl')
-import Data.Maybe (mapMaybe)
+import Data.Either (lefts, rights)
 import Util (median)
 
-weight :: Char -> Int
-weight ')' = 3
-weight ']' = 57
-weight '}' = 1197
-weight '>' = 25137
-
-part1 :: [String] -> Int 
-part1 = sum . map (go []) where
-    go _ [] = 0
+parseLine :: String -> Either Char [Char]
+parseLine = go [] where
+    go stack [] = Right stack
     go stack  (x:xs) | x `elem` "([{<"                        = go (x:stack) xs
     go (s:ss) (x:xs) | [s, x] `elem` ["()", "[]", "{}", "<>"] = go ss xs
-    go _ (x:xs) = weight x
+    go _ (x:_) = Left x
 
-weight2 :: Char -> Int
-weight2 '(' = 1
-weight2 '[' = 2
-weight2 '{' = 3
-weight2 '<' = 4
-
-stackWeight :: [Char] -> Int 
-stackWeight = foldl' (\acc x -> acc * 5 + weight2 x) 0
+part1 :: [String] -> Int
+part1 = sum . map weight . lefts . map parseLine where
+        weight ')' = 3
+        weight ']' = 57
+        weight '}' = 1197
+        weight '>' = 25137
 
 part2 :: [String] -> Int 
-part2 = median . mapMaybe (go []) where
-    go stack [] = Just $ stackWeight stack
-    go stack  (x:xs) | x `elem` "([{<"                        = go (x:stack) xs
-    go (s:ss) (x:xs) | [s, x] `elem` ["()", "[]", "{}", "<>"] = go ss xs
-    go _ (x:xs) = Nothing
+part2 = median . map stackWeight . rights . map parseLine where
+        weight '(' = 1
+        weight '[' = 2
+        weight '{' = 3
+        weight '<' = 4
+        stackWeight = foldl' (\acc x -> acc * 5 + weight x) 0
 
 solve :: String -> Maybe (Int, Int)
-solve s =
-    let ls = lines s in
-    pure (part1 ls, part2 ls)
+solve s = let ls = lines s in
+            pure (part1 ls, part2 ls)
