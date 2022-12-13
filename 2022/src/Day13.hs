f6a4f909f628c0a7fa7b5a9b9e568761e5e4bdb9
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2022-12-13 15:23:01 +0100

    day 13: find the solution of part 2 without sorting

--- 2022/src/Day13.hs
+++ 2022/src/Day13.hs
@@ -1,11 +1,10 @@
 -- https://adventofcode.com/2022/day/13
 module Day13 (solve) where
 import           RIO
-import           Data.List (elemIndex, sort)
 import           Text.Megaparsec (between, sepEndBy1, sepBy)
 import           Text.Megaparsec.Char (char, eol)
 import           Text.Megaparsec.Char.Lexer (decimal)
-import           Util (Parser, aoc)
+import           Util (Parser, aoc, count)
 
 data Packet = PInt Int | Packet [Packet] deriving (Eq)
 
@@ -24,14 +23,11 @@ parser = pair `sepEndBy1` (eol *> eol) where
 part1 :: [(Packet, Packet)] -> Int
 part1 = sum . zipWith (\i (p1, p2) -> if p1 <= p2 then i else 0) [1..]
 
-part2 :: [(Packet, Packet)] -> Maybe Int
-part2 pairs = do
-    let divider1 = Packet [Packet [PInt 2]]
-    let divider2 = Packet [Packet [PInt 6]]
-    let packets = sort $ [divider1, divider2] ++ (pairs >>= (\(p1, p2) -> [p1, p2]))
-    i1 <- elemIndex divider1 packets
-    i2 <- elemIndex divider2 packets
-    pure $ (i1+1) * (i2+1)
+part2 :: [(Packet, Packet)] -> Int
+part2 pairs = index1 * index2 where
+    packets = pairs >>= \(p1, p2) -> [p1, p2]
+    index1 = 1 + count (< Packet [Packet [PInt 2]]) packets
+    index2 = 2 + count (< Packet [Packet [PInt 6]]) packets
 
 solve :: (HasLogFunc env) => Text -> RIO env ()
 solve = aoc parser part1 part2
