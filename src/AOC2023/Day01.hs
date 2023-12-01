e9514b79be16a73958009a0e9bea2ec2a4c41b99
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-12-01 08:23:10 +0100

    day 1

--- src/AOC2023/Day01.hs
+++ src/AOC2023/Day01.hs
@@ -1,17 +1,38 @@
 -- https://adventofcode.com/2023/day/1
 module AOC2023.Day01 (solve) where
-import           RIO
-import           RIO.List (sort)
-import           Text.Megaparsec (sepEndBy1)
-import           Text.Megaparsec.Char (eol)
-import           Text.Megaparsec.Char.Lexer (decimal)
-import           Util (Parser, aoc, takeEnd)
+import           RIO hiding (some)
+import           RIO.Char (isDigit)
+import           RIO.List (isPrefixOf)
+import           RIO.List.Partial (head, last)
+import           RIO.Partial (read)
+import           Text.Megaparsec (sepEndBy1, some)
+import           Text.Megaparsec.Char (alphaNumChar, eol)
+import           Util (Parser, aoc)
 
-parser :: Parser [[Int]]
-parser = (decimal `sepEndBy1` eol) `sepEndBy1` eol
+parser :: Parser [String]
+parser = some alphaNumChar `sepEndBy1` eol
 
-solve' :: Int -> [[Int]] -> Int
-solve' n = sum . takeEnd n . sort . map sum
+solve' :: (String -> String) -> [String] -> Int
+solve' f = sum . map readInt where
+    readInt :: String -> Int
+    readInt s = let x = f s in read [head x, last x]
+
+filter1 :: String -> String
+filter1 = filter isDigit
+
+filter2 :: String -> String
+filter2 "" = ""
+filter2 (x:xs) | "one" `isPrefixOf` (x:xs) = '1' : filter2 xs
+               | "two" `isPrefixOf` (x:xs) = '2' : filter2 xs
+               | "three" `isPrefixOf` (x:xs) = '3' : filter2 xs
+               | "four" `isPrefixOf` (x:xs) = '4' : filter2 xs
+               | "five" `isPrefixOf` (x:xs) = '5' : filter2 xs
+               | "six" `isPrefixOf` (x:xs) = '6' : filter2 xs
+               | "seven" `isPrefixOf` (x:xs) = '7' : filter2 xs
+               | "eight" `isPrefixOf` (x:xs) = '8' : filter2 xs
+               | "nine" `isPrefixOf` (x:xs) = '9' : filter2 xs
+               | isDigit x = x : filter2 xs
+               | otherwise = filter2 xs
 
 solve :: MonadIO m => Text -> m ()
-solve = aoc parser (solve' 1) (solve' 3)
+solve = aoc parser (solve' filter1) (solve' filter2)
