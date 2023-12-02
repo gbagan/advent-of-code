b1ab8f5f985a63efdcf01407f61fccb0b15c4b55
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-12-03 00:21:50 +0100

    203 day 3: refactoring

--- src/AOC2023/Day03.hs
+++ src/AOC2023/Day03.hs
@@ -2,49 +2,41 @@
 module AOC2023.Day03 (solve) where
 import           RIO hiding (some)
 import           RIO.Partial (read)
+import           RIO.List.Partial (head, last)
 import           RIO.Char (isDigit)
 import           Text.Megaparsec (anySingleBut, sepEndBy1, some)
 import           Text.Megaparsec.Char (eol)
 import           Util (Parser, aoc)
-import           Util.Matrix (Matrix, (!), (!?), nbRows, nbColumns, fromList)
+import           Util.Matrix ((!?), fromList)
 
-type NumberRange = (Int, Int, Int) -- position of a number: row, start column, end column
+type NumberRange = (Int, Int, Int, Int) -- position of a number: row, start column, end column, the scanned number
 
-parser :: Parser (Matrix Char)
-parser = fromList <$> some (anySingleBut '\n') `sepEndBy1` eol
+parser :: Parser [[Char]]
+parser = some (anySingleBut '\n') `sepEndBy1` eol
 
-findNumberRanges :: Matrix Char -> [NumberRange]
-findNumberRanges mat = concatMap rangesInRow [0..m-1] where
-    rangesInRow i = reverse $ foldl' (go i) [] [0..n-1]
-    go i ranges j =
-        let c = mat ! (i, j) in
-        case ranges of
-            [] | isDigit c -> [(i, j, j)]
-               | otherwise -> []
-            (_, start, end):ts | isDigit c && end == j-1 -> (i, start, end+1) : ts
-                               | isDigit c -> (i, j, j) : ranges
-                               | otherwise -> ranges
-    n = nbColumns mat
-    m = nbRows mat
+findNumberRanges :: [[Char]] -> [NumberRange]
+findNumberRanges table = concatMap rangesInRow (zip [0..] table) where
+    rangesInRow (i, row) = go i (zip [0..] row)
+    go i js =
+        case break (isDigit . snd) js of
+            (_, []) -> []
+            (_, after) -> case span (isDigit . snd) after of
+                (before', after') -> (i, fst (head before'), fst (last before'), read @Int $ map snd before') : go i after'
 
-extractNumber :: Matrix Char -> NumberRange -> Int
-extractNumber mat (row, start, end) = read @Int [mat ! (row, c) | c <- [start..end]]
-
-solve1 :: Matrix Char -> Int
-solve1 mat = sum . map (extractNumber mat) . filter checkBorders $ findNumberRanges mat where
-    checkBorders (row, start, end) = any (maybe False isSymbol) [mat !? (r, c) | r <- [row-1..row+1], c <- [start-1..end+1]]
+solve1 :: [[Char]] -> Int
+solve1 table = sum . map (\(_, _, _, n) -> n) . filter checkBorders $ findNumberRanges table where
+    checkBorders (row, start, end, _) = any (maybe False isSymbol) [mat !? (r, c) | r <- [row-1..row+1], c <- [start-1..end+1]]
     isSymbol ch = not (isDigit ch || ch == '.')
+    mat = fromList table
 
-solve2 :: Matrix Char -> Int
-solve2 mat = sum (map gear positions) where
-    gear (r, c) = case adjacentRanges (r, c) of
-        [x, y] -> extractNumber mat x * extractNumber mat y
+solve2 :: [[Char]] -> Int
+solve2 table = sum (map gear positions) where
+    gear (r, c, ch) = case adjacentRanges r c ch of
+        [(_, _, _, x), (_, _, _, y)] -> x * y
         _ -> 0
-    adjacentRanges (r, c) = ranges & filter \(row, start, end) -> mat ! (r, c) == '*' && row-1 <= r && r <= row+1 && start-1 <= c && c <= end+1
-    ranges = findNumberRanges mat
-    positions = [(r, c) | r <- [0..m-1], c <- [0..n-1]]
-    n = nbColumns mat
-    m = nbRows mat
+    adjacentRanges r c ch = ranges & filter \(row, start, end, _) -> ch == '*' && row-1 <= r && r <= row+1 && start-1 <= c && c <= end+1
+    ranges = findNumberRanges table
+    positions = [(r, c, ch) | (r, row) <- zip [0..] table, (c, ch) <- zip [0..] row]
 
 solve :: MonadIO m => Text -> m ()
 solve = aoc parser solve1 solve2
