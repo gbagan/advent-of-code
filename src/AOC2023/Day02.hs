f41401433e8c79d4c439df4b2fdd34fdc3dcabdf
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-12-02 01:57:10 +0100

    2023 day 2

--- src/AOC2023/Day02.hs
+++ src/AOC2023/Day02.hs
@@ -1,17 +1,45 @@
 -- https://adventofcode.com/2023/day/2
 module AOC2023.Day02 (solve) where
 import           RIO
-import           RIO.List (sort)
 import           Text.Megaparsec (sepEndBy1)
-import           Text.Megaparsec.Char (eol)
+import           Text.Megaparsec.Char (char, eol, string)
 import           Text.Megaparsec.Char.Lexer (decimal)
-import           Util (Parser, aoc, takeEnd)
+import           Util (Parser, aoc)
 
-parser :: Parser [[Int]]
-parser = (decimal `sepEndBy1` eol) `sepEndBy1` eol
+data Color = Red | Green | Blue
 
-solve' :: Int -> [[Int]] -> Int
-solve' n = sum . takeEnd n . sort . map sum
+data Game = Game Int [(Int, Color)]
+
+parser :: Parser [Game]
+parser = game `sepEndBy1` eol where
+    game = do
+        _ <- string "Game "
+        id_ <- decimal
+        _ <- string ": "
+        sets_ <- set_ `sepEndBy1` (string "; " <|> string ", ")
+        return (Game id_ sets_)
+    set_ = do
+        n <- decimal
+        _ <- char ' '
+        color <- Blue <$ string "blue" <|> Red <$ string "red" <|> Green <$ string "green"
+        return (n, color)
+
+solve1 :: Int -> Int -> Int -> [Game] -> Int
+solve1 red green blue = sum . map solveGame where
+    solveGame (Game id_ sets_) | all isPossibleSet sets_ = id_
+                               | otherwise = 0
+    isPossibleSet (n, color) = case color of
+        Red -> n <= red
+        Green -> n <= green
+        Blue -> n <= blue
+
+solve2 :: [Game] -> Int
+solve2 = sum . map solveGame where
+    solveGame (Game _ sets_) = let (r, g, b) = foldl' go (0, 0, 0) sets_ in r * g * b
+    go (red, green, blue) (n, color) = case color of
+        Red -> (max red n, green, blue)
+        Green -> (red, max n green, blue)
+        Blue -> (red, green, max n blue)
 
 solve :: MonadIO m => Text -> m ()
-solve = aoc parser (solve' 1) (solve' 3)
+solve = aoc parser (solve1 12 13 14) solve2
