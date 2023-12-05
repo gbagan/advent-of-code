779c3d996c6cbfdbb68864f903db4cb95e5265bd
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-12-05 18:24:27 +0100

    day 5 simplify the code

--- src/AOC2023/Day05.hs
+++ src/AOC2023/Day05.hs
@@ -9,6 +9,7 @@ import           Text.Megaparsec.Char (char, string, eol)
 import           Text.Megaparsec.Char.Lexer (decimal)
 import           Util (Parser, aoc)
 
+type Interval = (Int, Int)
 type Range = (Int, Int, Int)
 type AMap = [Range]
 data Almanac = Almanac [Int] [AMap]
@@ -19,7 +20,35 @@ parser = Almanac <$> seeds <* eol <* eol <*> amap `sepEndBy1` eol where
     amap = takeWhileP Nothing (/= '\n') *> eol *> range `sepEndBy1` eol
     range = (,,) <$> decimal <* char ' ' <*> decimal <* char ' ' <*> decimal
 
+pairwise :: [a] -> [(a, a)]
+pairwise (x:y:xs) = (x, y) : pairwise xs
+pairwise _ = []
 
+solve' :: [AMap] -> [Interval] -> Int
+solve' maps seedIntervals = minimum (map fst finalIntervals) where
+    finalIntervals = foldl' go seedIntervals maps
+    go intervals ranges = catMaybes [nextInterval range itv | itv <- intervals, range <- fillRanges ranges]
+    nextInterval (dest, source, len) (begin, end) 
+        | end < source = Nothing
+        | source + len - 1 < begin = Nothing
+        | otherwise = Just $ both (+ (dest - source)) (max begin source, min end (source + len - 1))
+    fillRanges = filter ((/= 0) . thd3) . fillRanges' 0 . sortOn snd3
+    fillRanges' start [] = [(start, start, maxBound)]
+    fillRanges' start (x@(_, source, len) : xs) = (start, start, source-start) : x : fillRanges' (source+len) xs
+
+part1 :: Almanac -> Int
+part1 (Almanac seeds maps) = solve' maps [(s, s) | s <- seeds]
+
+part2 :: Almanac -> Int
+part2 (Almanac seeds maps) = solve' maps [(start, start+len-1) | (start, len) <- pairwise seeds]
+
+solve :: MonadIO m => Text -> m ()
+solve = aoc parser part1 part2
+
+
+
+
+{-
 travel :: [AMap] -> Int -> Int
 travel maps seed = foldl' destFromSource seed maps where
     destFromSource x ranges =
@@ -30,30 +59,6 @@ travel maps seed = foldl' destFromSource seed maps where
 part1 :: Almanac -> Int
 part1 (Almanac seeds maps) = minimum $ map (travel maps) seeds
 
-pairwise :: [a] -> [(a, a)]
-pairwise (x:y:xs) = (x, y) : pairwise xs
-pairwise _ = []
-
-part2 :: Almanac -> Int
-part2 (Almanac seeds maps) = minimum (map fst finalIntervals) where 
-    seeds' = [(start, start+len-1) | (start, len) <- pairwise seeds]
-    finalIntervals = foldl' go seeds' maps
-    go intervals ranges = catMaybes [nextInterval range itv | itv <- intervals, range <- fillRanges ranges]
-    nextInterval (dest, source, len) (begin, end) 
-        | end < source = Nothing
-        | source + len - 1 < begin = Nothing
-        | otherwise = Just $ both (+ (dest - source)) (max begin source, min end (source + len - 1))
-    fillRanges = filter ((/= 0) . thd3) . fillRanges' 0 . sortOn snd3
-    fillRanges' start [] = [(start, start, maxBound)]
-    fillRanges' start (x@(_, source, len) : xs) = (start, start, source-start) : x : fillRanges' (source+len) xs
-
-solve :: MonadIO m => Text -> m ()
-solve = aoc parser part1 part2
-
-
-
-
-{-
 part2 :: Almanac -> Maybe Integer
 part2 (Almanac seeds maps) = find isValid [approx-1000..approx+1000] where
     approx = fromJust $ find isValid [0,1000..]
