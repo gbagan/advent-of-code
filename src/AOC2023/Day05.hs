60f71ed283051a375e4ce194aa1889d436890c31
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-12-05 16:44:47 +0100

    day 5: clean the code

--- src/AOC2023/Day05.hs
+++ src/AOC2023/Day05.hs
@@ -1,15 +1,16 @@
 -- https://adventofcode.com/2023/day/5
 module AOC2023.Day05 (solve) where
 import           RIO hiding (some)
-import           RIO.List (find)
+import           RIO.List (sortOn)
 import           RIO.List.Partial (minimum)
-import           RIO.Partial (fromJust) 
+import           Data.Tuple.Extra (both, snd3, thd3)
 import           Text.Megaparsec (takeWhileP, sepBy1, sepEndBy1)
 import           Text.Megaparsec.Char (char, string, eol)
 import           Text.Megaparsec.Char.Lexer (decimal)
 import           Util (Parser, aoc)
 
-type AMap = [(Int, Int, Int)]
+type Range = (Int, Int, Int)
+type AMap = [Range]
 data Almanac = Almanac [Int] [AMap]
 
 parser :: Parser Almanac
@@ -30,11 +31,30 @@ part1 :: Almanac -> Int
 part1 (Almanac seeds maps) = minimum $ map (travel maps) seeds
 
 pairwise :: [a] -> [(a, a)]
-pairwise [] = []
-pairwise [_] = []
 pairwise (x:y:xs) = (x, y) : pairwise xs
+pairwise _ = []
 
-part2 :: Almanac -> Maybe Int
+part2 :: Almanac -> Int
+part2 (Almanac seeds maps) = minimum (map fst finalIntervals) where 
+    seeds' = [(start, start+len-1) | (start, len) <- pairwise seeds]
+    finalIntervals = foldl' go seeds' maps
+    go intervals ranges = catMaybes [nextInterval range itv | itv <- intervals, range <- fillRanges ranges]
+    nextInterval (dest, source, len) (begin, end) 
+        | end < source = Nothing
+        | source + len - 1 < begin = Nothing
+        | otherwise = Just $ both (+ (dest - source)) (max begin source, min end (source + len - 1))
+    fillRanges = filter ((/= 0) . thd3) . fillRanges' 0 . sortOn snd3
+    fillRanges' start [] = [(start, start, maxBound)]
+    fillRanges' start (x@(_, source, len) : xs) = (start, start, source-start) : x : fillRanges' (source+len) xs
+
+solve :: MonadIO m => Text -> m ()
+solve = aoc parser part1 part2
+
+
+
+
+{-
+part2 :: Almanac -> Maybe Integer
 part2 (Almanac seeds maps) = find isValid [approx-1000..approx+1000] where
     approx = fromJust $ find isValid [0,1000..]
     isValid = isSeed . travel reverseMaps
@@ -42,6 +62,4 @@ part2 (Almanac seeds maps) = find isValid [approx-1000..approx+1000] where
     isSeed x = seeds' & any \(start, end) -> start <= x && x <= end
     reverseMaps = reverse . map (map reverseMap) $ maps
     reverseMap (dest, source, len) = (source, dest, len)
-
-solve :: MonadIO m => Text -> m ()
-solve = aoc parser part1 part2
+-}
