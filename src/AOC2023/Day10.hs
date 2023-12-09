acb56d54e22f69973d116cd70782de0104d3c76a
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-12-10 00:48:06 +0100

    day 10 part 2

--- src/AOC2023/Day10.hs
+++ src/AOC2023/Day10.hs
@@ -8,44 +8,88 @@ import qualified Data.HashSet as Set
 import           AOC (aoc)
 import           AOC.Parser (Parser, choice, eol, sepEndBy1, some)
 import           AOC.Search (bfs)
-import           AOC.Util (count, listTo2dMap, adjacentPoints)
+import           AOC.Util (adjacentPoints)
 
-data Tile = NS | EW | NE | NW | SW | SE | NoPipe | Start deriving (Eq)
+data Tile = NS | EW | NE | NW | SW | SE | NoPipe | Start deriving (Eq, Show)
 type Input = [[Tile]]
+
+type Matrix = HashMap (Int, Int) Tile
+
+listTo2dMap :: [[a]] -> HashMap (Int, Int) a
+listTo2dMap l =
+    Map.fromList
+        [((i, j), v)
+        | (i, row) <- zip [0..] l
+        , (j, v) <- zip [0..] row
+        ]
 
 parser :: Parser Input
 parser =  some tile `sepEndBy1` eol where
     tile = choice [NS <$ "|", EW <$ "-", NE <$"L", NW <$ "J", SW <$ "7", SE <$ "F", NoPipe <$ ".", Start <$ "S"]
 
-loopBfs :: Input -> [(Int, (Int, Int))]
-loopBfs tiles = bfs neighbors start where
-    start = head [(i, j) | ((j, i), Start) <- Map.toList mat]
-    neighbors' (i, j) = case mat Map.!? (j, i) of
-        Just NS -> [(i-1, j), (i+1, j)]
-        Just EW -> [(i, j-1), (i, j+1)]
-        Just NE -> [(i-1, j), (i, j+1)]
-        Just NW -> [(i, j-1), (i-1, j)]
-        Just SW -> [(i+1, j), (i, j-1)]
-        Just SE -> [(i, j+1), (i+1, j)]
-        Just Start -> adjacentPoints (i, j)
-        _ -> []
-    neighbors (i, j) = neighbors' (i, j) & filter \pos -> (i, j) `elem` neighbors' pos
+cleanedInput :: Input -> (Input, Matrix, (Int, Int))
+cleanedInput tiles = (cleanedTiles, cleanedMat, start) where
+    start = head [pos | (pos, Start) <- Map.toList mat]
     mat = listTo2dMap tiles
+    goodTile = case [start `elem` neighbors mat nbor | nbor <- neighbors mat start] of
+        -- [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+        [True, True, False, False] -> NS
+        [False, False, True, True] -> EW
+        [True, False, False, True] -> NE
+        [True, False, True, False] -> NW
+        [False, True, False, True] -> SE
+        [False, True, True, False] -> SW
+        _ -> NoPipe  -- not possible
+    cleanedMat = Map.insert start goodTile mat
+    cleanedTiles = [ [ if tile == Start then goodTile else tile | tile <- row] 
+                   | row <- tiles
+                   ]
+
+
+neighbors :: Matrix -> (Int, Int) -> [(Int, Int)]
+neighbors mat (i, j) = case mat Map.!? (i, j) of
+    Just NS -> [(i-1, j), (i+1, j)]
+    Just EW -> [(i, j-1), (i, j+1)]
+    Just NE -> [(i-1, j), (i, j+1)]
+    Just NW -> [(i, j-1), (i-1, j)]
+    Just SW -> [(i+1, j), (i, j-1)]
+    Just SE -> [(i, j+1), (i+1, j)]
+    Just Start -> adjacentPoints (i, j)
+    _ -> []
+
+
+loopBfs :: Matrix -> (Int, Int) -> [(Int, (Int, Int))]
+loopBfs mat = bfs (neighbors mat)
 
 part1 :: Input -> Int
-part1 = maximum . map fst . loopBfs
+part1 tiles = maximum . map fst $ loopBfs mat start where 
+    (_, mat, start) = cleanedInput tiles
 
 keepOdds :: [a] -> [a]
 keepOdds (_:y:xs) = y : keepOdds xs
 keepOdds _ = []
 
 part2 :: Input -> Int
-part2 tiles = sum . map countRow $ tilesWithIndex where
-    countRow = sum . map (count notInLoop) . keepOdds . splitWhen isSeparator
-    notInLoop (i, j, _) =  not $ Set.member (j, i) loopSet
-    isSeparator (i, j, tile) = (i, j) `Set.member` loopSet && tile `elem` [NS, NW, NE, Start]
-    loopSet = Set.fromList . map snd $ loop tiles
-    tilesWithIndex = [[(i, j, tile) | (j, tile) <- zip [0..] row] | (i, row) <- zip [0..] tiles] 
+part2 tiles = sum . map countRow $ cleanedTiles where
+    (tiles', mat, start) = cleanedInput tiles
+    loopSet = Set.fromList . map snd $ loopBfs mat start
+    -- replace each tile not in the loop with "."
+    cleanedTiles = [ [ if (i, j) `Set.member` loopSet then tile else NoPipe
+                     | (j, tile) <- zip [0..] row
+                     ] 
+                   | (i, row) <- zip [0..] tiles'
+                   ]
+    countRow = sum 
+            . map length
+            . keepOdds
+            . splitWhen (`elem` [NS, NE, SE])
+            . filter (`notElem` [NW, SW])
+            . filter1
+            . filter (/= EW)   
+    filter1 (NE :  NW : xs) = filter1 xs
+    filter1 (SE : SW : xs) = filter1 xs
+    filter1 (x:xs) = x: filter1 xs
+    filter1 [] = []           
 
 solve :: Text -> IO ()
 solve = aoc parser part1 part2
