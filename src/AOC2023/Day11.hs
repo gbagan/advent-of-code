4a1f31d70dcde74415252a2e1b586c02072dd9ac
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-12-10 09:31:14 +0100

    day 11: code cleaning

--- src/AOC2023/Day11.hs
+++ src/AOC2023/Day11.hs
@@ -5,26 +5,27 @@ import qualified Data.Vector as V
 import           AOC (aoc)
 import           AOC.Parser (Parser, eol, sepEndBy1, some)
 
+type Coord = (Int, Int)
+
 parser :: Parser [[Bool]]
 parser = some isGalaxy `sepEndBy1` eol where
     isGalaxy = False <$ "." <|> True <$ "#"
 
+manhattan :: Coord -> Coord -> Int
+manhattan (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
 
-gridToList :: [[Bool]] -> [(Int, Int)]
+gridToList :: [[Bool]] -> [Coord]
 gridToList grid = [(i, j) | (i, row) <- zip [0..] grid, (j, isGalaxy) <- zip [0..] row, isGalaxy] 
 
 emptyLines :: [[Bool]] -> Vector Int
 emptyLines = V.fromList . drop 1 . scanl' (\acc row -> if all not row then acc+1 else acc) 0
 
 solveWith :: Int -> [[Bool]] -> Int
-solveWith expand grid = sum (manhattan <$> galaxies <*> galaxies) `div` 2 where
+solveWith expand grid = sum (manhattan <$> expGalaxies <*> expGalaxies) `div` 2 where
     galaxies = gridToList grid
     emptyRows = emptyLines grid
     emptyCols = emptyLines (transpose grid)
-    manhattan (x1, y1) (x2, y2) = dist + expandX + expandY where
-        dist = abs (x1 - x2) + abs (y1 - y2)
-        expandX = (expand - 1) * abs (emptyRows V.! x1 - emptyRows V.! x2)
-        expandY = (expand - 1) * abs (emptyCols V.! y1 - emptyCols V.! y2)
+    expGalaxies = [(x + (expand - 1) * emptyRows V.! x, y + (expand - 1) * emptyCols V.! y) | (x, y) <- galaxies]
 
 solve :: Text -> IO ()
 solve = aoc parser (solveWith 2) (solveWith 1000000)
