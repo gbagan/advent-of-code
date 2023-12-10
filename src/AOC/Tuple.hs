83a9c073c16c250a4d584b6b797e5a90d7273605
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-12-10 16:20:06 +0100

    fix warning on AOC.Tuple

--- src/AOC/Tuple.hs
+++ src/AOC/Tuple.hs
@@ -1,13 +1,13 @@
-module AOC.Tuple where
+    module AOC.Tuple where
 
 both :: (a -> b) -> (a, a) -> (b, b)
 both f (x, y) = (f x, f y)
 
 fst3 :: (a, b, c) -> a
-fst3 (a, b, c) = a
+fst3 (a, _, _) = a
 
 snd3 :: (a, b, c) -> b
-snd3 (a, b, c) = b
+snd3 (_, b, _) = b
 
 thd3 :: (a, b, c) -> c
-thd3 (a, b, c) = c
+thd3 (_, _, c) = c
