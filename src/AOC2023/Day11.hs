6312a28fa45ee7353a4eb05952d96ff4379ff176
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-12-10 11:28:19 +0100

    day 11: code cleaning

--- src/AOC2023/Day11.hs
+++ src/AOC2023/Day11.hs
@@ -6,25 +6,26 @@ import           AOC (aoc)
 import           AOC.Parser (Parser, eol, sepEndBy1, some)
 
 type Coord = (Int, Int)
+type Grid = [[Bool]]
 
-parser :: Parser [[Bool]]
+parser :: Parser Grid
 parser = some isGalaxy `sepEndBy1` eol where
     isGalaxy = False <$ "." <|> True <$ "#"
 
 manhattan :: Coord -> Coord -> Int
 manhattan (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
 
-gridToList :: [[Bool]] -> [Coord]
-gridToList grid = [(i, j) | (i, row) <- zip [0..] grid, (j, isGalaxy) <- zip [0..] row, isGalaxy] 
+gridToList :: Grid -> [Coord]
+gridToList grid = [(i, j) | (i, row) <- zip [0..] grid, (j, True) <- zip [0..] row] 
 
-emptyLines :: [[Bool]] -> Vector Int
-emptyLines = V.fromList . drop 1 . scanl' (\acc row -> if all not row then acc+1 else acc) 0
+countEmptyRows :: Grid -> Vector Int
+countEmptyRows = V.fromList . drop 1 . scanl' (\acc row -> if all not row then acc+1 else acc) 0
 
-solveWith :: Int -> [[Bool]] -> Int
+solveWith :: Int -> Grid -> Int
 solveWith expand grid = sum (manhattan <$> expGalaxies <*> expGalaxies) `div` 2 where
     galaxies = gridToList grid
-    emptyRows = emptyLines grid
-    emptyCols = emptyLines (transpose grid)
+    emptyRows = countEmptyRows grid
+    emptyCols = countEmptyRows (transpose grid)
     expGalaxies = [(x + (expand - 1) * emptyRows V.! x, y + (expand - 1) * emptyCols V.! y) | (x, y) <- galaxies]
 
 solve :: Text -> IO ()
