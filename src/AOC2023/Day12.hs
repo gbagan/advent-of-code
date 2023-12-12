0ebf30c4f8ecbaf3a2bfa59dcfa77ff299105ee4
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-12-12 11:07:41 +0100

    day 12: make some optimizations

--- src/AOC2023/Day12.hs
+++ src/AOC2023/Day12.hs
@@ -1,6 +1,7 @@
 -- https://adventofcode.com/2023/day/12
 module AOC2023.Day12 (solve) where
 import           AOC.Prelude
+import           Data.List (maximum)
 import           AOC (aoc)
 import           AOC.Parser (Parser, sepEndBy1, some, eol, decimal, hspace)
 import qualified Data.Vector as V
@@ -24,21 +25,23 @@ compute (Row springs lengths) = arr ! (0, 0, 0) where
     arr = listArray bds [
         if pos == springLen then
             if lenPos == lenLen then 1 else 0
-        else 
+        else
             let currentSpring = vsprings V.! pos in
             if lenPos == lenLen then
                 if currentSpring == Damaged then 0 else arr ! (pos + 1, lenPos, 0)
             else
                 sum $ [Operational, Damaged] <&> \s ->
                     if | currentSpring `notElem` [s, Unknown] -> 0
-                       | s == Damaged -> arr ! (pos + 1, lenPos, currentLengthPos + 1)
+                       | s == Damaged -> 
+                            if | currentLengthPos == vLengths V.! lenPos -> 0
+                               | otherwise -> arr ! (pos + 1, lenPos, currentLengthPos + 1)
                        | otherwise -> -- s == Operational
                             if | currentLengthPos == vLengths V.! lenPos -> arr ! (pos + 1, lenPos + 1, 0)
                                | currentLengthPos == 0 -> arr ! (pos + 1, lenPos, 0)
-                               |  otherwise -> 0
+                               | otherwise -> 0
         | (pos, lenPos, currentLengthPos) <- range bds
         ]
-    bds = ((0, 0, 0), (springLen, lenLen, springLen))
+    bds = ((0, 0, 0), (springLen, lenLen, maximum lengths))
 
 part1 :: [Row] -> Integer
 part1 = sum . map compute
