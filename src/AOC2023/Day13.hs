4046c87d0c0f617b00f908a7c8f024f4aafe369f
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-12-13 15:44:28 +0100

    day 13: some optimizations

--- src/AOC2023/Day13.hs
+++ src/AOC2023/Day13.hs
@@ -5,7 +5,6 @@ import           AOC (aoc)
 import qualified Data.Vector as V
 import           Data.Vector ((!))
 import           AOC.Parser (Parser, sepEndBy1, some, eol)
-import           AOC.Util (count)
 
 data Tile = Ash | Rock deriving (Eq)
 type Grid = [[Tile]]
@@ -13,22 +12,26 @@ type Grid = [[Tile]]
 parser :: Parser [Grid]
 parser = (some tile `sepEndBy1` eol) `sepEndBy1` eol where
     tile = Ash <$ "." <|> Rock <$ "#"
-
-difference :: [Tile] -> [Tile] -> Int
-difference l1 l2 = count id $ zipWith (/=) l1 l2
-
+ 
 isSymetry :: Int -> Vector [Tile] -> Int -> Bool
-isSymetry diff v x = total == diff where
-    total = sum $ range <&> \i -> difference (v ! i) (v ! (2 * x - i - 1))
+isSymetry nbDiffs v x = lengthEq nbDiffs difference where
+    difference = filter id $ zipWith (/=) list1 list2
+    list1 = concatMap (v!) range
+    list2 = concatMap (\i -> v ! (2 * x - i - 1)) range
     n = V.length v
     range = if 2 * x < n then [0..x-1] else [x..n-1]
 
 solveFor :: Int -> [Grid] -> Int
-solveFor diff = sum . map score where
-    score grid = 100 * symetries grid + symetries (transpose grid)
-    symetries grid = find (isSymetry diff vgrid) [1..n-1] ?: 0 where
+solveFor nbDiffs = sum . map score where
+    score grid = (100*) <$> symetry grid <|> symetry (transpose grid) ?: 0  
+    symetry grid = find (isSymetry nbDiffs vgrid) [1..n-1] where
         vgrid = V.fromList grid
         n = V.length vgrid
+
+lengthEq :: Int -> [a] -> Bool
+lengthEq n [] = n == 0
+lengthEq 0 _ = False
+lengthEq n (_:xs) = lengthEq (n-1) xs
 
 solve :: Text -> IO ()
 solve = aoc parser (solveFor 0) (solveFor 1)
