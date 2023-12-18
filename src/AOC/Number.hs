fa663094f3c726a64de451061e18295d9f48dffb
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-12-18 20:42:06 +0100

    2020 day 13

--- src/AOC/Number.hs
+++ src/AOC/Number.hs
@@ -2,20 +2,48 @@ module AOC.Number where
 
 import AOC.Prelude
 
--- extgcd a b
--- Return the gcd of a and b, along with x and y such that
--- gcd(a,b) = ax + by
+-- Return (g, x, y) such that g is the gcd of a and b
+-- and ax + by = gcd(a,b)
 extgcd :: Integral a => a -> a -> (a, a, a)
 extgcd 0 0 = error "extgcd 0 0 is undefined"
-extgcd a 0 = (1,0,a)
-extgcd a b = (x, c-q*x, y) where 
-    (q,r) = a `eDivMod` b
-    (c,x,y) = extgcd b r
+extgcd a 0 = (a, 1, 0)
+extgcd a b = (g, y, x-q*y) where 
+    (q,r) = a `divMod` b
+    (g, x, y) = extgcd b r
 
--- minv a n
--- Returns the modular inverse of a mod n. Assumes that a and n are coprime.
-modularInverse a n =
-  | g == 1    = Just $ a*x + n*y
-  | otherwise = Nothing
-  where
-    (x, y, g) = extgcd a n
+{-# SPECIALISE extgcd :: Integer -> Integer -> (Integer, Integer, Integer) #-}
+
+-- Return the modular inverse of a mod n.
+modularInverse :: Integral a => a -> a -> Maybe a
+modularInverse a n
+    | g == 1    = Just $ x `mod` n
+    | otherwise = Nothing
+    where (g, x, _) = extgcd a n
+
+{-# SPECIALISE modularInverse :: Integer -> Integer -> Maybe Integer #-}
+
+{-
+chineseRemainder :: Integral a => [(a, a)] -> Maybe (a, a)
+chineseRemainder = foldlM go (0, 1) where
+    go (r1, m1) (r2, m2) = do
+        im2 <- modularInverse m2 m1
+        let r = r2 + (r1 - r2) * m2 * im2
+        let m = m1 * m2
+        return (r `mod` m, m)
+-}
+
+-- Given a list of (ri, mi)
+-- returns a tuple (q, m) where {q + j m | j in Z} is the set of solutions
+-- of the equations x = ri (mod mi)
+-- It is not necessary that all mi are pairwise coprime
+-- returns Nothing if there is no solution
+chineseRemainder :: Integral a => [(a, a)] -> Maybe (a, a)
+chineseRemainder = foldlM go (0, 1) where
+    go (a, m) (b, n) = do
+        let (g, u, v) = extgcd m n
+        guard $ (a - b) `mod` g == 0
+        let x = (a * v * n + b * u * m) `div` g
+        let m' = (m * n) `div` g
+        return (x `mod` m', m')
+
+{-# SPECIALISE chineseRemainder :: [(Integer, Integer)] -> Maybe (Integer, Integer) #-}
--- src/AOC2020/Day13.hs
+++ src/AOC2020/Day13.hs
@@ -4,18 +4,22 @@ import           AOC.Prelude
 import           Data.List (minimum)
 import           AOC (aoc)
 import           AOC.Parser (Parser, sepEndBy1, eol, decimal, sepEndBy1)
+import           AOC.Number (chineseRemainder)
 
-data Input = Input Int [Maybe Int] 
+data Input = Input Integer [Maybe Integer] 
 
 parser :: Parser Input
 parser = Input <$> decimal <* eol <*> period `sepEndBy1` "," where
     period = Just <$> decimal <|> Nothing <$ "x"
 
-part1 :: Input -> Int
+part1 :: Input -> Integer
 part1 (Input earliest periods) = uncurry (*) $ minimum [((-earliest) `mod` p, p) | p <- catMaybes periods]
 
-part2 :: Input -> Int
-part2 (Input earliest periods) = 0
+part2 :: Input -> Maybe Integer
+part2 (Input _ periods) = fst <$> chineseRemainder pairs where
+    pairs = mapMaybe f $ zip [0..] periods
+    f (i, Just a) = Just (-i, a)
+    f _ = Nothing
 
 solve :: Text -> IO ()
 solve = aoc parser part1 part2
