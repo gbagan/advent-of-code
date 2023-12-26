d493060b82c7f9b842b9ceb1073990f776c34728
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-12-26 05:23:21 +0100

    2015 day 5 and 6

---
+++ src/2015/Day05.hs
@@ -0,0 +1,25 @@
+-- https://adventofcode.com/2015/day/5
+module Day05 (solve) where
+import           AOC.Prelude
+import           AOC (aoc)
+import           Data.List (isInfixOf)
+import           AOC.Parser (Parser, eol, some, lowerChar, sepEndBy1)
+import           AOC.List (count)
+
+parser :: Parser [String]
+parser = some lowerChar `sepEndBy1` eol
+
+check1 :: String -> Bool
+check1 s = count (`elem` ['a', 'e', 'i', 'o', 'u']) s >= 3
+        && any ((>=2) . length) (group s)
+        && not (any ( `isInfixOf` s) ["ab", "cd", "pq", "xy"])
+
+check2 :: String -> Bool
+check2 s = check21 s && check22 s where
+    check21 (x : y : xs) = [x, y] `isInfixOf` xs || check21 (y : xs)
+    check21 _ = False
+    check22 (x : y : z : xs) = x == z || check22 (y : z : xs) 
+    check22 _ = False
+
+solve :: Text -> IO ()
+solve = aoc parser (count check1) (count check2)
---
+++ src/2015/Day06.hs
@@ -0,0 +1,54 @@
+-- https://adventofcode.com/2015/day/6
+module Day06 (solve) where
+import           AOC.Prelude hiding (toList)
+import           AOC (aoc)
+import           AOC.Parser (Parser, sepEndBy1, eol, hspace, decimal)
+import           Control.Monad.ST (ST)
+import           Data.Massiv.Array (U, Sz(..), Ix2(..), toList)
+import           Data.Massiv.Array.Mutable
+import           AOC.List (count)
+
+data Command = Toggle | On | Off deriving (Show)
+data Instruction = Instruction !Command !Int !Int !Int !Int deriving (Show)
+
+parser :: Parser [Instruction]
+parser = instruction `sepEndBy1` eol where
+    instruction = do
+        cmd <- On <$ "turn on" <|> Off <$ "turn off"<|> Toggle <$ "toggle"
+        x1 <- hspace *> decimal
+        y1 <- "," *> decimal
+        x2 <- " through " *> decimal
+        y2 <- "," *> decimal
+        pure $ Instruction cmd x1 y1 x2 y2
+
+
+runInstruction :: MArray r U Ix2 Bool -> Instruction -> ST r ()
+runInstruction arr (Instruction cmd x1 y1 x2 y2) = do
+    for_ [x1..x2] \i ->
+        for_ [y1..y2] \j ->
+            case cmd of
+                On     -> write_ arr (Ix2 i j) True
+                Off    -> write_ arr (Ix2 i j) False
+                Toggle -> modify_ arr (pure . not) (Ix2 i j)
+
+part1 :: [Instruction] -> Int
+part1 instructions = count id . toList $ createArrayST_ @U (Sz2 1000 1000) \arr -> do
+    for_ instructions (runInstruction arr)
+
+runInstruction2 :: MArray r U Ix2 Int -> Instruction -> ST r ()
+runInstruction2 arr (Instruction cmd x1 y1 x2 y2) = do
+    for_ [x1..x2] \i ->
+        for_ [y1..y2] \j ->
+            case cmd of
+                On     -> modify_ arr (pure . succ) (Ix2 i j)
+                Off    -> modify_ arr (pure . max 0 . pred) (Ix2 i j)
+                Toggle -> modify_ arr (pure . (+2)) (Ix2 i j)
+
+part2 :: [Instruction] -> Int
+part2 instructions = sum . toList $ createArrayST_ @U (Sz2 1000 1000) \arr -> do
+    for_ instructions (runInstruction2 arr)
+
+solve :: Text -> IO ()
+solve = aoc parser part1 part2
+
+-- 17325717 too low
