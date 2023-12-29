9113a7b9a7b3ae92cbb9bb423f047b8a9b154afa
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-12-29 18:28:26 +0100

    2015 start day 11

---
+++ src/2016/Day11.hs
@@ -0,0 +1,80 @@
+-- https://adventofcode.com/2016/day/11
+module Day11 (solve) where
+import           AOC.Prelude hiding (State, state)
+import           AOC (aoc)
+import           AOC.Parser (Parser, eol, sepEndBy1, sepBy1, some, letterChar, format)
+import qualified Data.Text as Text
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Instances ()
+import           AOC.List (grouped2, pairwise)
+import           AOC.Graph (distance)
+
+data ItemType = Generator | Microchip deriving (Eq, Ord, Show)
+data Item = Item !Text !ItemType  deriving (Eq, Ord, Show)
+
+data State = State Int (V.Vector Int) deriving (Eq, Show)
+
+instance Hashable State where
+    hashWithSalt s (State x xs) = s `hashWithSalt` x `hashWithSalt` xs
+
+    -- human floor, pairs of generator, microchip positions
+
+subsets2 :: [a] -> [[a]]
+subsets2 xs = [[]] ++ map pure xs ++ pairwise (\x y -> [x, y]) xs
+
+parser :: Parser [[Item]]
+parser = row `sepEndBy1` eol where
+    row = snd <$> [format|The {nth} floor contains {components}.|]
+    nth = "first" <|> "second" <|> "third" <|> "fourth"
+    components = [] <$"nothing relevant"
+                <|> component `sepBy1` (", and " <|> ", ")
+    component = Item <$> ("a " *> label) <*> itemtype
+    itemtype = Generator <$ " generator" <|> Microchip <$ "-compatible microchip"
+    label = Text.pack <$> some letterChar
+
+isValid :: State -> Bool
+isValid (State _ items) = all isValid' indexed where
+    isValid' (idx, (gen, chip)) = 0 <= gen && gen <= 3 && 0 <= chip && chip <= 3
+                        && (gen == chip
+                            || all (\(idx', (gen', _)) -> idx == idx' || chip /= gen') indexed
+                           )
+    indexed = zip [(0::Int)..] . grouped2 $ V.toList items
+
+atSameFloor :: State -> [Int]
+atSameFloor (State human items) = [ i | (i, f) <- zip [0..] (V.toList items), f==human]
+
+neighbors :: State -> [State]
+neighbors state@(State human items) = states where
+    states =
+        [ state' 
+        | direction <- [1, - 1]
+        , let human' = human + direction
+        , c <- subsets2 (atSameFloor state)
+        , let items' = items V.// map (, human') c
+        , let state' = State (human + direction) items'
+        , isValid state'
+        ]
+
+startingState :: [[Item]] -> State
+startingState items = State 0 items'' where
+    items'' = V.fromList . map snd $ sortOn fst [ (item, i)
+                                                | (i, items') <- zip [0..] items
+                                                , item <- items'
+                                                ]
+
+isDest :: State -> Bool
+isDest (State _ items) = all (==3) (V.toList items)
+
+part1 :: [[Item]] -> Int
+part1 items = traceShow (startingState items) 0
+
+part2 :: [[Item]] -> Maybe Int
+part2 items = distance neighbors isDest (startingState items)
+
+solve :: Text -> IO ()
+solve = aoc parser part1 part2
+
+-- The first floor contains a strontium generator, a strontium-compatible microchip, a plutonium generator, and a plutonium-compatible microchip.
+-- The second floor contains a thulium generator, a ruthenium generator, a ruthenium-compatible microchip, a curium generator, and a curium-compatible microchip.
+-- The third floor contains a thulium-compatible microchip.
+-- The fourth floor contains nothing relevant.
