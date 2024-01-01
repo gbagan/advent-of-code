c874239495c746c2fca2b1d202787bf2678ae7fc
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2024-01-01 13:05:08 +0100

    2016 day 22, continue part 2

--- src/2016/Day22.hs
+++ src/2016/Day22.hs
@@ -1,19 +1,25 @@
 -- https://adventofcode.com/2016/day/22
 module Day22 (solve) where
-import           AOC.Prelude
+import           AOC.Prelude hiding (State)
 import           AOC (aoc)
 import           AOC.Parser (Parser, decimal, eol, format, hspace, sepEndBy1, skipLine)
 import           AOC.List (groupOn)
-import           AOC.V2 (V2(..))
+import           AOC.V2 (V2(..), manhattan, origin, adjacent)
+import           Data.Massiv.Array hiding (map, dropWhile)
+import           AOC.Area (Area)
+import qualified AOC.Area as Area
+impor            AOC.Search (astar)
 
 data Node = Node { _used, _avail :: !Int}
 type Input = [(V2 Int, Node)]
+
+data State = State { _goal, _hole :: !(V2 Int) }
 
 parser :: Parser Input
 parser = skipLine *> skipLine *> node `sepEndBy1` eol where
     node = do
         (x, y, _, used, avail, _) <- [format|/dev/grid/node-x{d}-y{d}{d}T{d}T{d}T{d}%|]
-        pure (V2 y x, Node used avail)
+        pure (V2 x y, Node used avail)
     d = hspace *> decimal
 
 part1 :: Input -> Int
@@ -26,8 +32,22 @@ part1 nodes = aux useds avails len 0 where
     aux (u:us) (a:as) l total | u <= a    = aux us (a:as) l $! total + l 
                               | otherwise = aux (u:us) as (l-1) $! total 
 
+inputToMat :: Input -> Matrix B Node
+inputToMat input = fromLists' Seq . map (map snd) $ groupOn (\(V2 x _, _) -> x) input       
+
+neighbors :: Area Int -> State -> [(State, Int)]
+neighbors area (State goal hole) = 
+    [ (State goal' hole', 1)
+    | hole' <- adjacent hole
+    , hole' `Area.elem` area
+    , let goal' = if hole' == goal then goal' else goal
+    ]
+
+heuristic :: State -> Int
+heuristic (State goal hole) = manhattan goal origin + manhattan goal hole - 1
+
 part2 :: Input -> Int
-part2 _ = 0
+part2 input = 0
 
 solve :: Text -> IO ()
 solve = aoc parser part1 part2
